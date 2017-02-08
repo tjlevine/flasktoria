6bbab20872ebe938f8cacbaa2627b9de17d3f748
Tyler Levine <tylevine@cisco.com>
2017-02-08 15:10:12 -0800

    Fix copy-paste error

--- docker-entry.sh
+++ docker-entry.sh
@@ -4,12 +4,8 @@
 # so that the broker bootstrap process can complete successfully
 ENT="10.0.1.214      pnda-clb-cluster-kafka-0"
 
-if [ -z "$ENT" ]; then
-    echo "Did not find address for pnda-proxy container, will not be able to connect to kafka"
-else
-    echo "Adding entry to /etc/hosts: $ENT"
-    echo $ENT >> /etc/hosts
-fi
+echo "Adding entry to /etc/hosts: $ENT"
+echo $ENT >> /etc/hosts
 
 # start the app
 cd /flasktoria
