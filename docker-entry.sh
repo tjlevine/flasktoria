b30bd2a70de573e4aeac426ca26d3ea80a556d63
Tyler Levine <tylevine@cisco.com>
2017-02-08 15:25:07 -0800

    Add datanode address to /etc/hosts

--- docker-entry.sh
+++ docker-entry.sh
@@ -3,9 +3,12 @@
 # add the pnda-proxy container IP to /etc/hosts as pnda-clb-cluster-kafka-0
 # so that the broker bootstrap process can complete successfully
 ENT="10.0.1.214      pnda-clb-cluster-kafka-0"
+ENT2="10.0.1.124      pnda-clb-cluster-cdh-dn-0"
 
 echo "Adding entry to /etc/hosts: $ENT"
 echo $ENT >> /etc/hosts
+echo "Adding entry to /etc/hosts: $ENT2"
+echo $ENT2 >> /etc/hosts
 
 # start the app
 cd /flasktoria
