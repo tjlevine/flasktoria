0ea8acae9e0026a2b64c576a032737bdd9272bd3
Tyler Levine <tylevine@cisco.com>
2017-02-09 10:43:54 -0800

    Fix docker entry point (it was overwritten with deploy version)

--- docker-entry.sh
+++ docker-entry.sh
@@ -2,13 +2,14 @@
 
 # add the pnda-proxy container IP to /etc/hosts as pnda-clb-cluster-kafka-0
 # so that the broker bootstrap process can complete successfully
-ENT="10.0.1.214      pnda-clb-cluster-kafka-0"
-ENT2="10.0.1.124      pnda-clb-cluster-cdh-dn-0"
+ENT=$(getent hosts pnda-proxy | sed 's/pnda-proxy/pnda-clb-cluster-kafka-0/')
 
-echo "Adding entry to /etc/hosts: $ENT"
-echo $ENT >> /etc/hosts
-echo "Adding entry to /etc/hosts: $ENT2"
-echo $ENT2 >> /etc/hosts
+if [ -z "$ENT" ]; then
+    echo "Did not find address for pnda-proxy container, will not be able to connect to kafka"
+else
+    echo "Adding entry to /etc/hosts: $ENT"
+    echo $ENT >> /etc/hosts
+fi
 
 # start the app
 cd /flasktoria
