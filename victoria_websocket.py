0e5c352bcd180b14e3c71af26a6cf436cc56e049
Tyler Levine <tylevine@cisco.com>
2017-02-14 00:39:28 -0800

    Get rid of multiprocessing stuff, just poll consumers from ws handler loop

--- victoria_websocket.py
+++ victoria_websocket.py
@@ -6,6 +6,17 @@ import asyncio
 import queue
 import functools
 import multiprocessing
+import avro
+import avro.schema
+import avro.io
+import io
+import itertools
+import os
+import websockets
+import signal
+import sys
+from kafka import KafkaConsumer
+from kafka.structs import TopicPartition
 
 import anomalies
 
@@ -13,70 +24,6 @@ from cfg import cfg
 
 log = logging.getLogger("flasktoria.ws")
 log.setLevel(logging.INFO)
-
-def start_kafka_consumer(msg_queue):
-    from kafka import KafkaConsumer
-    import avro
-    import avro.schema
-    import avro.io
-    import io
-
-    log = logging.getLogger('flasktoria.ws.sensor')
-    log.setLevel(logging.INFO)
-
-    topic = cfg("KAFKA_TOPIC")
-    avro_path = cfg("AVRO_SCHEMA_PATH")
-    kafka_bootstrap_server = cfg("KAFKA_BOOTSTRAP_SERVER")
-    kafka_group_id = cfg("KAFKA_GROUP_ID")
-
-    with open(avro_path) as fin:
-        avro_schema = avro.schema.Parse(fin.read())
-
-    log.debug("kafka bootstrap server: %s", kafka_bootstrap_server)
-    consumer = KafkaConsumer(topic, group_id=kafka_group_id, bootstrap_servers=[kafka_bootstrap_server], auto_offset_reset='latest', enable_auto_commit=False)
-
-    for msg in consumer:
-        try:
-            bytes_reader = io.BytesIO(msg.value)
-            decoder = avro.io.BinaryDecoder(bytes_reader)
-            reader = avro.io.DatumReader(avro_schema)
-            record = reader.read(decoder)
-            #log.debug("Received sensor message: {}".format(record))
-            msg_queue.put(record)
-        except AssertionError:
-            log.warn("Got assertion error from avro decode on message: %s", msg)
-
-def start_anomaly_kafka_consumer(msg_queue):
-    from kafka import KafkaConsumer
-    import avro
-    import avro.schema
-    import avro.io
-    import io
-
-    log = logging.getLogger('flasktoria.ws.anomaly')
-    log.setLevel(logging.INFO)
-
-    topic = cfg("KAFKA_ANOMALY_TOPIC")
-    avro_path = cfg("AVRO_ANOMALY_SCHEMA_PATH")
-    kafka_bootstrap_server = cfg("KAFKA_BOOTSTRAP_SERVER")
-    kafka_group_id = cfg("KAFKA_GROUP_ID")
-
-    with open(avro_path) as fin:
-        avro_schema = avro.schema.Parse(fin.read())
-
-    log.debug("kafka bootstrap server: %s", kafka_bootstrap_server)
-    consumer = KafkaConsumer(topic, group_id=kafka_group_id, bootstrap_servers=[kafka_bootstrap_server], auto_offset_reset='latest', enable_auto_commit=False)
-
-    for msg in consumer:
-        try:
-            bytes_reader = io.BytesIO(msg.value)
-            decoder = avro.io.BinaryDecoder(bytes_reader)
-            reader = avro.io.DatumReader(avro_schema)
-            record = reader.read(decoder)
-            #log.debug("Received anomaly message: {}".format(record))
-            msg_queue.put(record)
-        except AssertionError:
-            log.warn("Got assertion error from avro decode on message: %s", msg)
 
 def parse_gps_message(msg, wsctl_dict):
     #value = json.loads(msg['value'])['value']
@@ -157,41 +104,63 @@ def get_kafka_updates(msg_queue):
         except queue.Empty:
             return updates
 
-def filter_suppressed_messages(wsctl_dict, updates):
-    updates = list(filter(lambda x: x is not None, updates))
-    return updates
+
+def get_sensor_consumer(ws_ctr):
+    topic = cfg("KAFKA_TOPIC")
+    schema_path = cfg("AVRO_SCHEMA_PATH")
+    group_id = cfg("KAFKA_GROUP_ID")
+    bootstrap_server = cfg("KAFKA_BOOTSTRAP_SERVER")
+
+    with open(schema_path) as fin:
+        avro_schema = avro.schema.Parse(fin.read())
+
+    log.debug("kafka bootstrap server: %s", bootstrap_server)
+    return KafkaConsumer(topic, group_id=group_id + str(ws_ctr), bootstrap_servers=[bootstrap_server], auto_offset_reset='latest', enable_auto_commit=False)
+
+def get_anomaly_consumer(ws_ctr):
+    topic = cfg("KAFKA_ANOMALY_TOPIC")
+    schema_path = cfg("AVRO_ANOMALY_SCHEMA_PATH")
+    group_id = cfg("KAFKA_GROUP_ID")
+    bootstrap_server = cfg("KAFKA_BOOTSTRAP_SERVER")
+
+    with open(schema_path) as fin:
+        avro_schema = avro.schema.Parse(fin.read())
+
+    log.debug("kafka bootstrap server: %s", bootstrap_server)
+    return KafkaConsumer(topic, group_id=group_id + str(ws_ctr), bootstrap_servers=[bootstrap_server], auto_offset_reset='latest', enable_auto_commit=False)
+
+def deser_sensor_message(msg):
+    schema_path = cfg("AVRO_SCHEMA_PATH")
+    with open(schema_path) as fin:
+        avro_schema = avro.schema.Parse(fin.read())
+    try:
+        bytes_reader = io.BytesIO(msg.value)
+        decoder = avro.io.BinaryDecoder(bytes_reader)
+        reader = avro.io.DatumReader(avro_schema)
+        record = reader.read(decoder)
+        #log.debug("Received anomaly message: {}".format(record))
+        return record
+    except AssertionError:
+        log.warn("Got assertion error from avro decode on message: %s", msg)
+
+def deser_anomaly_message(msg):
+    schema_path = cfg("AVRO_ANOMALY_SCHEMA_PATH")
+    with open(schema_path) as fin:
+        avro_schema = avro.schema.Parse(fin.read())
+    try:
+        bytes_reader = io.BytesIO(msg.value)
+        decoder = avro.io.BinaryDecoder(bytes_reader)
+        reader = avro.io.DatumReader(avro_schema)
+        record = reader.read(decoder)
+        #log.debug("Received anomaly message: {}".format(record))
+        return record
+    except AssertionError:
+        log.warn("Got assertion error from avro decode on message: %s", msg)
 
 def ws_main(wsctl_dict):
-    import os
-    import websockets
-    import asyncio
-    import signal
-    import sys
-
     log.debug('in ws main (pid is %d)', os.getpid())
 
-    # start the kafka consumer process
-    mgr = multiprocessing.Manager()
-    kafka_msg_queue = mgr.Queue()
-    kafka_anomaly_queue = mgr.Queue()
-
-    kafka_process = multiprocessing.Process(target=start_kafka_consumer, args=(kafka_msg_queue,))
-    kafka_process.start()
-    log.debug("kafka process: %s", kafka_process)
-
-    kafka_anomaly_process = multiprocessing.Process(target=start_anomaly_kafka_consumer, args=(kafka_anomaly_queue,))
-    kafka_anomaly_process.start()
-    log.debug("kafka anomaly process: %s",kafka_anomaly_process)
-
     def sigterm_handler(signum, frame):
-        log.debug("WS server is shutting down")
-        kafka_process.terminate()
-        log.debug("kafka process terminated")
-        kafka_anomaly_process.terminate()
-        log.debug("kafka anomaly process terminated")
-        kafka_anomaly_process.join()
-        kafka_process.join()
-        log.debug("kafka processes joined")
         asyncio.get_event_loop().stop()
         log.debug("asyncio event loop stopped")
         log.info("Websocket server terminating")
@@ -203,53 +172,81 @@ def ws_main(wsctl_dict):
     _, _, port = cfg('WS_URL').split(':')
     host = "0.0.0.0"
     log.info('starting ws server on %s:%s', host, port)
+    wsctl_dict['ctr'] = 0
 
     # define our emit loop as a closure so we can access wsctl_dict
     async def emit_loop(ws, path):
-        log.debug("entering event loop")
-        while True:
-            #updates = test_data.get_test_updates()
-            updates = get_kafka_updates(kafka_msg_queue)
-            sensor_updates = len(updates)
-            updates += get_kafka_updates(kafka_anomaly_queue)
-            anomaly_updates = len(updates) - sensor_updates
-            log.info("Got %d sensor updates and %d anomaly updates this loop", sensor_updates, anomaly_updates)
+        log.info("New websocket connection established")
 
-            if len(updates) > 0:
-                #kafka_cache.add_entries(updates)
+        running = True
+        sensor_consumer = get_sensor_consumer(wsctl_dict['ctr'])
+        anomaly_consumer = get_anomaly_consumer(wsctl_dict['ctr'])
+        sensor_topic = cfg("KAFKA_TOPIC")
+        anomaly_topic = cfg("KAFKA_ANOMALY_TOPIC")
+        wsctl_dict['ctr'] += 1
 
+        while running:
+            sensor_records = sensor_consumer.poll()
+            topic_partition = TopicPartition(topic=sensor_topic, partition=0)
+            if topic_partition in sensor_records:
+                sensor_updates = map(deser_sensor_message, sensor_records[topic_partition])
+                num_sensor_updates = len(sensor_records[topic_partition])
+            else:
+                log.debug('Missing key %s in sensor records poll dict, probably no messages arrived', sensor_topic)
+                num_sensor_updates = 0
+                sensor_updates = []
+
+            anomaly_records = anomaly_consumer.poll()
+            topic_partition = TopicPartition(topic=anomaly_topic, partition=0)
+            if len(anomaly_records) > 0:
+                anomaly_updates = map(deser_anomaly_message, anomaly_records[topic_partition])
+                num_anomaly_updates = len(anomaly_records[topic_partition])
+            else:
+                log.debug('Missing key %s in anomaly records poll dict, probably no messages arrived', anomaly_topic)
+                num_anomaly_updates = 0
+                anomaly_updates = []
+
+            log.info("Got %d sensor updates and %d anomaly updates this loop", num_sensor_updates, num_anomaly_updates)
+
+            if num_sensor_updates + num_anomaly_updates > 0:
+                updates = itertools.chain(sensor_updates, anomaly_updates)
                 # map the kafka messages into their equivalent parsed format
                 bound_parse_fn = functools.partial(parse_kafka_message, wsctl_dict)
-                updates = list(map(bound_parse_fn, updates))
+                updates = map(bound_parse_fn, updates)
 
-                log.debug("Post parsing:")
-                for message in updates:
-                    log.debug(message)
+                if log.isEnabledFor(logging.DEBUG):
+                    updates = list(updates)
+                    log.debug("Post parsing:")
+                    for message in updates:
+                        log.debug(message)
 
                 # filter out the messages we don't want to send over the websocket
-                updates = filter_suppressed_messages(wsctl_dict, updates)
+                #updates = filter(lambda x: x is not None, updates)
 
-                log.debug("Post filter:")
-                for message in updates:
-                    log.debug(message)
+                if log.isEnabledFor(logging.DEBUG):
+                    log.debug("Post filter:")
+                    for message in updates:
+                        log.debug(message)
                 
-                updates = list(map(lambda m: m[1], updates))
-                messages = {"updates": updates}
+                updates = map(lambda m: m[1], updates)
+                messages = {"updates": list(updates)}
 
-                log.debug("Sending these messages over websocket:")
-                for message in updates:
-                    log.debug(message)
+                if log.isEnabledFor(logging.DEBUG):
+                    log.debug("Sending these messages over websocket:")
+                    for message in messages['updates']:
+                        log.debug(message)
 
-                log.info("emitting %d updates", len(updates))
+                log.info("emitting %d updates", len(messages['updates']))
 
                 # send the remaining messages over the websocket connection
                 try:
                     await ws.send(json.dumps(messages))
                 except websockets.exceptions.ConnectionClosed:
-                    log.info("Failed to send data over websocket, websocket is closed")
-
+                    log.info("Websocket is closing")
+                    running = False
 
             await asyncio.sleep(1)
+        log.info("exiting websocket handler, websocket is closed")
 
     # set up the ws server
     start_server = websockets.serve(emit_loop, host, port)
